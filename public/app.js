0b270fbec13f20a648fa0d50eea243be93f6ab87
mykhailodanilenko <mykhailo.danilenko@gmail.com>
2021-02-26 00:00:35 +0200

    wip

--- public/app.js
+++ public/app.js
@@ -10,11 +10,13 @@ function toggleMenu(e){
     menu.classList.remove('menu-out');
     dark.classList.remove('menu-out');
     line.classList.remove('line-active');
+    document.body.style.overflow = '';
     } else {
         burger.classList.add('open');
         menu.classList.add('menu-out');
         dark.classList.add('menu-out');
         line.classList.add('line-active');
+        document.body.style.overflow = "hidden";
         }
 }
 
@@ -23,3 +25,29 @@ menuButtons.forEach(Element => {
     Element.addEventListener('click', toggleMenu);
 });
 dark.addEventListener('click', toggleMenu);
+
+// scroll to top
+let scrollToTopBtn = document.getElementById('scrollToTopBtn');
+let target = document.querySelector('#ab');
+function callback(entries, observer) {
+    // The callback will return an array of entries, even if you are only observing a single item
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Show button
+        scrollToTopBtn.classList.remove('showBtn');
+      } else {
+        // Hide button
+        scrollToTopBtn.classList.add('showBtn');
+      }
+    });
+  }
+  let observer = new IntersectionObserver(callback);
+  observer.observe(target);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+  scrollToTopBtn.addEventListener("click", scrollToTop);
